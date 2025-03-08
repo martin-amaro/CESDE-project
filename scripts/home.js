7aa30dce56105cea109a5634d10f515cb855eba0
Martin Amaro <martingadpacks@gmail.com>
2025-03-08 09:40:14 -0500

    feat: faq section

--- scripts/home.js
+++ scripts/home.js
@@ -16,6 +16,7 @@ menuToggle.addEventListener('click', (event) => {
 
 });
 
+// Partners
 const root = document.documentElement;
 const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
 const marqueeContent = document.querySelector("ul.partner-gallery__content");
@@ -26,6 +27,7 @@ for(let i=0; i<marqueeElementsDisplayed; i++) {
 marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
 }
 
+// Parallax	Effect
 const handleScroll = () => {
     const scrollTop = document.documentElement.scrollTop || window.scrollY;
     const parallaxImage = document.querySelector('.parallax2');
@@ -37,3 +39,19 @@ const handleScroll = () => {
 };
 
 document.addEventListener('scroll', handleScroll);
+
+// FAQ
+document.querySelectorAll(".faq-title").forEach(title => {
+    title.addEventListener("click", function() {
+        const content = this.nextElementSibling;
+        const isActive = content.classList.contains("active");
+
+        document.querySelectorAll(".faq-content").forEach(c => c.classList.remove("active"));
+        document.querySelectorAll(".faq-title").forEach(t => t.classList.remove("active"));
+
+        if (!isActive) {
+            content.classList.add("active");
+            this.classList.add("active");
+        }
+    });
+});
